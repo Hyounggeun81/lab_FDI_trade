a2e2206056fb1bb1c73e9e590cd0c86e2ad4a8aa
Hyounggeun Cho <hyc039@ucsd.edu>
2026-05-23 02:10:06 +0900

    Convert import queries to export queries

--- lab_FDI_trade.R
+++ lab_FDI_trade.R
@@ -5,86 +5,35 @@
 #### Version 1.2 
  
 
-
 # clear environment
 rm(list = ls())
 
- 
-# new packages we need for Census and BEA
 
 #install.packages("censusapi")
 library(censusapi)
-#install.packages('bea.R')
 library(tidyverse)
 
-
-## Part 3: Census Trade Data ----
-
-
-#naics basis
-imports_naics<-getCensus(
-  name = "timeseries/intltrade/imports/naics", # this says where to look there are dozens of options
-  vars = c("GEN_VAL_MO"), #this is the variable you want: here General Imports by Month
-  time = "from 2016",
-  CTY_CODE="1220", #this is Canada, Census uses it's own 4 character codes
-  CTY_CODE="2010", #this is China
-  show_call = TRUE #useful to check or for replication later on different system
-)
-head(imports_naics)
-
-# note there is also a cumulative import and expor value with suffix YR
-imports_naics<-getCensus(
-  name = "timeseries/intltrade/imports/naics",
-  vars = c("GEN_VAL_YR","GEN_VAL_MO","YEAR"),
-  time = "from 2023",
-  CTY_CODE="1220",
-  CTY_CODE="2010"
-)
- 
-
-#to really save time, we want the cumulative value, for the month of December
-#GEN_VAL_YR is cumulative imports for consumption by month
-#the end of the year value for this is annual total, month=12
-imports_naics<-getCensus(
-  name = "timeseries/intltrade/imports/naics",
-  vars = c("GEN_VAL_YR","YEAR"),
-  time = "from 2016",
-  CTY_CODE="1220",
-  CTY_CODE="2010",
-  MONTH = "12", ## this setting here only gets us year end values #
-)
-
-## I want a make a graph of the top 10 import partners for any given year
-
-## so we will use the method above, but we want all countries
-## we also need to screen out some regional codes again
-## We also want to get the country names because the numericacodes 
-# are not meaningful to non-specialists
-
-imports_cty_yr<-getCensus(
-  name = "timeseries/intltrade/imports/naics",
-  vars = c("GEN_VAL_YR","YEAR","CTY_CODE","CTY_NAME"),
-  time = "from 2000",
+# Get annual U.S. export data by country
+exports_cty_yr<-getCensus(
+  name = "timeseries/intltrade/exports/naics",
+  vars = c("ALL_VAL_YR", "YEAR", "CTY_CODE", "CTY_NAME"),
+  time = "from 2015",
   MONTH = "12",
   show_call = TRUE
 )
+head(exports_cty_yr)
 
-
-head(imports_cty_yr)
-
-
-#filter region and other aggregation codes#
-#takea  look at the data you can see why I do this#
-imports_cty_yr_clean <- imports_cty_yr %>%
+# Filter out region and aggregation codes
+exports_cty_yr_clean <- exports_cty_yr %>%
   filter(!(substr(CTY_CODE, 1, 1) == "0" | substr(CTY_CODE, 2, 2) == "X" | substr(CTY_CODE,1,1)=="-"))
 
-#also, the values for year and imports are not numeric
-#we want to change that and convert to billions of dollars#
-imports_cty_yr_clean <- imports_cty_yr_clean %>%
-  mutate(GEN_VAL_YR = as.numeric(GEN_VAL_YR)/1000000000,
+# Convert export values to billions of dollars
+exports_cty_yr_clean <- exports_cty_yr_clean %>%
+  mutate(ALL_VAL_YR = as.numeric(ALL_VAL_YR)/1000000000,
          YEAR = as.numeric(YEAR))
+
 # Check for possible introduction of NAs due to conversion errors
-sum(is.na(imports_cty_yr_clean$GEN_VAL_YR))
+sum(is.na(exports_cty_yr_clean$ALL_VAL_YR))
 
 #a different way to sort top 10 than we did for BEA
 #here we use slice_max
